4a3eb936740b9d26c863988f597c28d6c516359c
Jakub Laník <kubanecxxx@gmail.com>
2012-08-03 17:48:38 +0200

    init correction

--- fonts.h
+++ fonts.h
@@ -1,22 +1,15 @@
-#ifndef __FONTS_H
-#define __FONTS_H
+/*
+ * fonts.h
+ *
+ *  Created on: 24.5.2012
+ *      Author: kubanec
+ */
 
-/* Includes ------------------------------------------------------------------*/
-#include <stdint.h>
+#ifndef FONTS_H_
+#define FONTS_H_
 
-typedef struct _tFont
-{    
-  const uint16_t *table;
-  uint16_t Width;
-  uint16_t Height;
-  
-} sFONT;
 
-extern sFONT Font16x24;
-extern sFONT Font12x12;
-extern sFONT Font8x12;
-extern sFONT Font8x8;
+const unsigned char * Fonts_GetChar (unsigned char character,unsigned char size);
 
-#define LINE(x) ((x) * (((sFONT *)LCD_GetFont())->Height))
 
-#endif /* __FONTS_H */
+#endif /* FONTS_H_ */
